dcb6044e9de7cf0df2c8c87d7829155be572b0e2
Nick Embrey <cne.embrey@gmail.com>
2017-07-15 22:01:13 -0700

    Add foundation & jquery to project.

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,22 @@ As always, I'm here if you run into any issue or have any questions.
 
 - Andrew */
 
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './app/app.jsx',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery',
+    })
+  ],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
